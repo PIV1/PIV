68e4d240cf753296d5334c49a8f4b3da13c9c077
PIV1 <felipecarlos@piv.one>
2026-04-18 13:35:21 -0600

    Create privacidad.js

---
+++ pages/privacidad.js
@@ -0,0 +1,44 @@
+export default function Privacidad() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+
+      <h1>Aviso de Privacidad</h1>
+
+      <p>
+        PIV (en adelante, “la Empresa”), con domicilio en Av. Palomas #196, Col. Las Palomas, Xalisco, Nayarit, es responsable del uso y protección de los datos personales que recaba de sus clientes y prospectos.
+      </p>
+
+      <p>
+        Los datos personales que recabamos de usted serán utilizados para las siguientes finalidades:
+      </p>
+
+      <ul>
+        <li>Dar seguimiento a solicitudes de información sobre nuestros proyectos inmobiliarios</li>
+        <li>Establecer contacto con clientes potenciales</li>
+        <li>Brindar atención personalizada y asesoría comercial</li>
+        <li>Enviar información relevante sobre nuestros desarrollos</li>
+      </ul>
+
+      <p>
+        Para las finalidades antes mencionadas, podemos recabar datos personales como nombre, número telefónico, correo electrónico y cualquier otra información que usted proporcione voluntariamente a través de nuestros medios de contacto.
+      </p>
+
+      <p>
+        La Empresa no comparte sus datos personales con terceros, salvo que sea estrictamente necesario para cumplir con las finalidades antes descritas o por requerimiento legal de autoridad competente.
+      </p>
+
+      <p>
+        Usted tiene derecho a acceder, rectificar o cancelar sus datos personales, así como oponerse al uso de los mismos (derechos ARCO). Para ejercer estos derechos, puede enviar una solicitud al correo electrónico: hola@piv.one
+      </p>
+
+      <p>
+        La Empresa se reserva el derecho de realizar modificaciones o actualizaciones al presente Aviso de Privacidad en cualquier momento. Dichas modificaciones estarán disponibles en este mismo sitio web.
+      </p>
+
+      <p>
+        Fecha de última actualización: abril 2026
+      </p>
+
+    </main>
+  );
+}
