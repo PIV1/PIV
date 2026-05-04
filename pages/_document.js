14e9f640bea78f3f48f4c13645a5da913fe2169f
PIV1 <felipecarlos@piv.one>
2026-05-03 21:35:50 -0600

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <link
+          rel="canonical"
+          href="https://www.piv.one/desarrollador-inmobiliario-tepic"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
